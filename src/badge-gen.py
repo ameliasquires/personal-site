d6bf7cfd425d2399356cd76acdb3751dd38568c6
amy <ameliasquires@disroot.org>
2023-02-02 20:48:29 -0600

    Update badge-gen.py

--- src/badge-gen.py
+++ src/badge-gen.py
@@ -3,4 +3,4 @@ directory = './badges'
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     if os.path.isfile(f):
-        print("<img alt=\""+f+"\"  title=\""+f+"\" src=\""+f+"\">")
+        print("<img alt=\""+f+"\"  title=\""+f+"\" src=\""+f+"\">",end =" ")
